480616ebb317916c92f1c69029e1090e63dcf4b5
Mert <yigittopm@hotmail.com>
2023-06-16 11:21:21 +0300

    esm syntax added

---
+++ 02-the-module-system/09-esm-syntax/main3.js
@@ -0,0 +1,5 @@
+import { log, Logger } from "./logger.js"
+
+log("Hello world")
+const logger = new Logger("DEFAULT")
+logger.log("Hello world")
---
+++ 02-the-module-system/09-esm-syntax/main5.js
@@ -0,0 +1,6 @@
+import { log as log2 } from "./logger.js"
+
+const log = console.log
+
+log("message from log")
+log2("message from log2")
